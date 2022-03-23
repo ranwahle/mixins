0a1889d5d4adb57b0672e71997eb2c0a812e2470
Ran <ran.wahle@gmail.com>
2022-03-23 09:11:45 +0200

    Rearange apply-mixins

--- apply-mixins.ts
+++ apply-mixins.ts
@@ -1,49 +1,50 @@
+type AConstructor = new (...args: any[]) => {}
+
 // Each mixin is a traditional ES class
 class ALivingRoom {
-    playWithKids() {}
+  playWithKids() {}
 
-    watchTv() {}
+  watchTv() {}
 
-    eatDinner() {
-
-    }
+  eatDinner() {}
 }
 
 class AShower {
-    takeAShower() {}
+  takeAShower() {}
 }
-   
-  // Including the base
-  class ToiletSeat {
-   iDontWannaKnow() {
-       console.log(`I don't wanna know`)
-   }
-  }
-   
-  // Then you create an interface which merges
-  // the expected mixins with the same name as your base
-  interface ToiletSeat extends ALivingRoom, AShower {}
-  // Apply the mixins into the base class via
-  // the JS at runtime
-  function applyMixins(derivedCtor: any, constructors: any[]) {
-    constructors.forEach((baseCtor) => {
-      Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
-        Object.defineProperty(
-          derivedCtor.prototype,
-          name,
-          Object.getOwnPropertyDescriptor(baseCtor.prototype, name) ||
-            Object.create(null)
-        );
-      });
-    });
-  }   
-  const myLivongRoom = new ToiletSeat();
-  myLivongRoom.playWithKids();
-  myLivongRoom.iDontWannaKnow();
 
-  const myShower = new ToiletSeat();
-  myShower.takeAShower();
-  myShower.iDontWannaKnow();
-   
-  
-  
+// Including the base
+class ToiletSeat {
+  iDontWannaKnow() {
+    console.log(`I don't wanna know`);
+  }
+}
+
+// Then you create an interface which merges
+// the expected mixins with the same name as your base
+interface ToiletSeat extends ALivingRoom, AShower {}
+
+// Apply the mixins into the base class via
+// the JS at runtime
+function applyMixins(derivedCtor: AConstructor, constructors: AConstructor[]) {
+  constructors.forEach((baseCtor) => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+      Object.defineProperty(
+        derivedCtor.prototype,
+        name,
+        Object.getOwnPropertyDescriptor(baseCtor.prototype, name) ||
+          Object.create(null)
+      );
+    });
+  });
+}
+
+applyMixins(ToiletSeat, [ALivingRoom, AShower])
+
+const myLivongRoom = new ToiletSeat();
+myLivongRoom.playWithKids();
+myLivongRoom.iDontWannaKnow();
+
+const myShower = new ToiletSeat();
+myShower.takeAShower();
+myShower.iDontWannaKnow();
